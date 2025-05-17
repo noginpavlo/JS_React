0b3c449002cb7062d314448f1612425317ad52a5
highlander-95 <nogin.pavlo@lll.kpi.ua>
2025-05-17 12:51:30 +0200

    Created variables.js and learned about arrow functions that are commonly used in React

---
+++ basic_syntax/functions.js
@@ -0,0 +1,18 @@
+function greet(name) {
+  console.log("Hello, " + name + "!");
+}
+
+
+function add(a,b) {
+    const sum = a + b;
+    return sum
+}
+
+
+const square = x => x * x;
+
+
+console.log(square(2))
+console.log(add(1, 2));
+greet("Alice"); // Output: Hello, Alice!
+
