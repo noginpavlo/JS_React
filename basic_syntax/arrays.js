e96fc73d8e263595103864cff3cd71a2b7f287a0
highlander-95 <nogin.pavlo@lll.kpi.ua>
2025-05-17 13:07:37 +0200

    Learned objects in JS whish is the same as python dicionary basically

--- basic_syntax/arrays.js
+++ basic_syntax/arrays.js
@@ -16,3 +16,24 @@ console.log("Now for loop will iterate over all animals in animals array")
 for (let animal of animals) {
     console.log(animal)
 }
+
+
+const user = {
+  name: "Alice",
+  age: 30,
+  isActive: true
+};
+
+console.log(user.name);      // "Alice"
+console.log(user["age"]);    // 30
+
+const tasks = [
+  { title: "Learn JS", done: true },
+  { title: "Start React", done: false },
+  { title: "Build project", done: false }
+];
+
+console.log("All tasks:");
+for (let task of tasks) {
+  console.log("-", task.title, "Done?", task.done);
+}
