e9d448bed4ffa6d62d09df979cb0e70f5b4101e1
highlander-95 <nogin.pavlo@lll.kpi.ua>
2025-05-17 12:58:54 +0200

    Learned arrays which are the same thing as python list and their basic methods

---
+++ basic_syntax/arrays.js
@@ -0,0 +1,12 @@
+const animals = ["cat", "dog", "bird"];
+console.log(animals[0]);
+console.log(animals[1]);
+console.log(animals[2]);
+console.log(animals);
+
+
+
+console.log(animals.length);
+animals.push("crocodile")
+console.log(animals)
+console.log(animals.length);
