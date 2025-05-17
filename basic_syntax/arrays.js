364d57bb86a92d8f8a7c1027fc4cc82e91a9963a
highlander-95 <nogin.pavlo@lll.kpi.ua>
2025-05-17 13:03:05 +0200

    learned for loops

--- basic_syntax/arrays.js
+++ basic_syntax/arrays.js
@@ -10,3 +10,9 @@ console.log(animals.length);
 animals.push("crocodile")
 console.log(animals)
 console.log(animals.length);
+
+
+console.log("Now for loop will iterate over all animals in animals array")
+for (let animal of animals) {
+    console.log(animal)
+}
