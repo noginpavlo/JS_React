3f613e1dc22ef64b59b401460b225d3fb8cdb539
highlander-95 <nogin.pavlo@lll.kpi.ua>
2025-05-20 18:02:00 +0200

    Learned conditional logic in JS

---
+++ basic_syntax/conditionals.js
@@ -0,0 +1,9 @@
+const age = 18;
+
+if (age > 18) {
+    console.log("You are and adult")
+} else if (age < 18) {
+    console.log("You are not an adult")
+} else {
+    console.log("You are exactly 18 years old so you are and adult, it's a shame that noone will take thata seriosly")
+}
