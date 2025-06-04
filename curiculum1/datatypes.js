2ef95c15c26b428b45b19c1f1b698c641fd034bc
highlander-95 <nogin.pavlo@lll.kpi.ua>
2025-06-04 15:13:04 +0200

    Practiced JS datatypes inside datatypes.js

---
+++ curiculum1/datatypes.js
@@ -0,0 +1,52 @@
+// // immutale data types in JS
+// console.log(typeof "hello world");
+// console.log(typeof 199);
+// console.log(typeof true);
+// console.log(typeof null);
+//
+// // Arrays (lists) 
+// console.log("Here is an array");
+// const people = ["Valintin", "Grishania", "Serega Svarka"];
+// console.log(people);
+// console.log("Here is an item from the array under specific intex");
+// console.log(people[2]);
+//
+// // Arrays (objects) == the same thing as Python dictionaries
+// const users = {
+//   name: "Big name in this world",
+//   age: 100,
+//   isAutenticated: true,
+// };
+
+
+// Task 1
+const siteName = "My site";
+const version = 1.0;
+console.log(siteName, version)
+// Task 2
+let isLoggedIn = false;
+isLoggedIn = true;
+console.log(isLoggedIn);
+
+// Task 3
+const tasks = ["to do the dishes", "make my bed", "code"];
+console.log(tasks[1]);
+
+// Task 4
+const profile = {
+  name: "Pavlo",
+  age: 29,
+  isActive: true,
+}
+console.log(profile.name);
+console.log(profile.age);
+console.log(profile.isActive);
+
+// Task 5
+const users = [
+  { username: "motherfucker3000", email: "motherfucker3000@gmail.com" },
+  { username: "goodguy03", email: "goodguy03@gmail.com" },
+]
+console.log(users[1].username);
+console.log(users[1].email);
+
