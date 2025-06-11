567facebcecdfa90809efcda80fb6df81d8cc68d
highlander-95 <nogin.pavlo@lll.kpi.ua>
2025-06-11 15:03:16 +0200

    Learned how functions are defined in React with arrow

--- curiculum1/functions.js
+++ curiculum1/functions.js
@@ -1,6 +1,21 @@
+// Almost Python. The simplest way to define function
 function greet(name) {
-  return `Hello, {name}!`
+  return `Hello, {name}!`;
 }
 
-console.log(greet("Pavlo"))
+console.log(greet("Pavlo"));
 
+// Something like defining a variable and store a function in this variable
+// Basically it is function declaration
+const addItem = function(item) {
+  return `Added {item} item!`;
+}
+
+console.log(addItem("Book"));
+
+// This is the same as above, but the 'function' keyword is replaced by arrow =>
+const saveThing = (thing) => {
+  return `Thing {thing} is saved!`;
+}
+
+console.log(saveThing("Computer"));
