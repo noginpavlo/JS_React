ab29046750753807d123bace6bbfec8a046fd75f
highlander-95 <nogin.pavlo@lll.kpi.ua>
2025-06-11 15:23:47 +0200

    Solved a couple of tasks inside JS functions topic. For React

--- curiculum1/functions.js
+++ curiculum1/functions.js
@@ -19,3 +19,30 @@ const saveThing = (thing) => {
 }
 
 console.log(saveThing("Computer"));
+
+
+// Task 3
+console.log("Task 3:");
+
+const add = (a, b) => {
+  return a + b;
+}
+console.log(add(1, 2));
+
+
+// Task 4
+console.log("Task 4:");
+
+const isEven = (number) => number % 2 === 0;
+
+console.log(isEven(2))
+console.log(isEven(3))
+
+// Task 5
+console.log("Task 5:");
+
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+const doubledNumbers = nums => nums.map(n => n * 2);
+
+console.log(doubledNumbers(numbers));
+
