098d72410297baa91a664213d0b9752f3044fb9a
highlander-95 <nogin.pavlo@lll.kpi.ua>
2025-06-12 18:40:59 +0200

    Studied better way of writing for loops in js => for ... of

--- curiculum1/loops.js
+++ curiculum1/loops.js
@@ -1,10 +1,18 @@
 // Arrays and loops
 
-// For loop is crazy in JS man
-
+// For loop is crazy in JS man (basicaly it is unreadable version of for loop)
+console.log("Unreadabel loop:")
 numbers = [0, 1, 2, 3, 4, 5, 6, 7]
 
-for (let i = 0; i <= numbers.length; i++) {
+for (let i = 0; i < numbers.length; i++) {
   console.log(numbers[i])
+}
+
+//for ... of => this is readable version of for loop in JS
+console.log("Readable loop:")
+const fruits = ["lemon", "apple", "orange", "chery"]
+
+for (const fruit of fruits) {
+  console.log(fruit)
 }
 
